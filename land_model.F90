75127baeff7bae22fa69a70017b75b1b06affc4f
Uriel Ramirez <uriel.ramirez@noaa.gov>
2025-03-03 10:34:34 -0500

    minimum changes needed for the gex updates

--- land_model.F90
+++ land_model.F90
@@ -133,12 +133,15 @@ type :: atmos_land_boundary_type
         wind      => NULL(), &   ! abs wind speed at the bottom of the atmos, m/s
         z_bot     => NULL(), &   ! height of the bottom atmospheric layer above the surface, m
         drag_q    => NULL(), &   ! product of cd_q by wind
-        p_surf    => NULL()      ! surface pressure, Pa
+        p_surf    => NULL(), &   ! surface pressure, Pa
+        con_atm   => NULL()      ! conductance between atmosphere and canopy, m/s
 
    real, dimension(:,:,:,:), pointer :: & ! (lon, lat, tile, tracer)
         tr_flux => NULL(),   &   ! tracer flux, including water vapor flux
         dfdtr   => NULL()        ! derivative of the flux w.r.t. tracer surface value, 
                                  ! including evap over surface specific humidity
+
+   real, dimension(:,:,:), pointer :: gex_atm2lnd => NULL() !generic exchange fields
 
    integer :: xtype             !REGRID, REDIST or DIRECT
 end type atmos_land_boundary_type
@@ -161,6 +164,8 @@ type :: land_data_type
 
    real, pointer, dimension(:,:,:,:)   :: &  ! (lon, lat, tile, tracer)
         tr    => NULL()              ! tracers, including canopy air specific humidity
+
+   real, pointer, dimension(:,:,:) :: gex_lnd2atm => NULL() ! (grid_index, tile, gex field)
 
    real, pointer, dimension(:,:) :: &  ! (lon, lat)
      discharge           => NULL(),  & ! liquid water flux from land to ocean
